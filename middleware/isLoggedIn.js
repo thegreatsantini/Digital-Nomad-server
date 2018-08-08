6651477fe4f46da669dfe0a75f2146e53a13481c
thegreatsantini <lss13@my.fsu.edu>
2018-08-07 19:45:26 -0700

    removed old files from hackathon, changed get user data route

--- middleware/isLoggedIn.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(req, res, next) {
-	if(!req.user) {
-		req.flash('error', 'You are not authorized to view this page. Please login.');
-		res.redirect('/auth/login');
-	}
-	else {
-		next();
-	}
-}
