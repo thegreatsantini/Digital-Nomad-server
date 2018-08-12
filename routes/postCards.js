39fa9b5cfc12687f08b7af714cd034d550493f65
thegreatsantini <lss13@my.fsu.edu>
2018-08-11 21:58:31 -0700

    testing;

--- routes/postCards.js
+++ routes/postCards.js
@@ -14,7 +14,7 @@ router.get('/test/:id', function (req, res) {
 })
 // Render the page with the wishlist form
 router.get('/api/v1/:id', function (req, res) {
-	console.log('*******************************')
+	
 	sentCards.find({userId:req.params.id}, function (error, cards) {
 		if (error) res.status(404).send('couldn\'t GET user info');
 		else {
